44e50e9d3bba772a090f3a8b5508f0f1352cfc97
yokozawa <y.yokozawa@gmail.com>
2013-12-18 21:14:57 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petty::Application.config.secret_key_base = 'e263bccc079d0e684d0d4f4c57631ce94217bb82869244d2b8bb9e9a2f4dcf8d8d3279d4ec5256f2ce8c7c1f8252f5ad369856e4cba913079c98dab6a8ad9ddb'
