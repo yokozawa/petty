bcc161776e4384c04d5e26b2b3f20832fc851f6e
yokozawa <y.yokozawa@gmail.com>
2013-12-30 16:41:06 +0900

    --

--- app/models/details.rb
+++
@@ -1,5 +0,0 @@
-class Details < ActiveRecord::Base
-  belongs_to :user_id
-  belongs_to :type
-  belongs_to :meta
-end
--- test/models/details_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DetailsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
