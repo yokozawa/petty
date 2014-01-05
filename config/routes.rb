aa0ddad9093fbb9686b1cc3aabbf20ddd4da56fc
yokozawa <y.yokozawa@gmail.com>
2014-01-05 22:37:11 +0900

    --

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Petty::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'summary#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
