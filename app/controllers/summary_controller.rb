21e5167937ff3a57c96840d526ce895dc1a7c75f
yokozawa <y.yokozawa@gmail.com>
2014-01-16 21:58:21 +0900

    --

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -26,20 +26,20 @@ class SummaryController < ApplicationController
     @types.each do |type|
       name = sprintf("@recs_%d_%d", type.id, INCOME)
       @recs = Detail.get_records_by_filter(current_user.id, type.id, INCOME, date)
-      eval("#{name} = @recs")
+      instance_variable_set("#{name}", @recs)
       @recs.each do |rec|
         @income_sum += rec['amount'] ? rec['amount'] : 0 
       end
 
       name = sprintf("@recs_%d_%d", type.id, OUTGO)
       @recs = Detail.get_records_by_filter(current_user.id, type.id, OUTGO, date)
-      eval("#{name} = @recs")
+      instance_variable_set("#{name}", @recs)
       sum = 0
       @recs.each do |rec|
         sum += rec['amount'] ? rec['amount'] : 0
       end
       sum_name = sprintf("@outgo_sum_%d", type.id)
-      eval("#{sum_name} = sum")
+      instance_variable_set("#{sum_name}", sum)
     end
   end
 end
