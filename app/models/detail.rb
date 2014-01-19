60bce92a2ad5f4c479c9703a3c69a2bb0b4c6685
yokozawa <y.yokozawa@gmail.com>
2014-01-19 17:24:23 +0900

    --

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -39,8 +39,7 @@ class Detail < ActiveRecord::Base
 
     today = Date.today
     payment_date = sprintf("%04d-%02d-%02d", today.next_month.year, today.next_month.month, self.type.payment_day)
-    rec = Detail.find(:conditions 
-      => {:user_id => self.user_id, :type_id => self.type.id, :record_at => payment_day}).first
+    rec = Detail.find(:conditions => {:user_id => self.user_id, :type_id => self.type.id, :record_at => payment_day}).first
 
     if type.cutoff_day == GETSUMATSU
       cutoff_date = sprintf("%04d-%02d-%02d", today.next_month.year, today.next_month.month, -1)
